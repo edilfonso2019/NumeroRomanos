f517c548ad6404894c154d10b49b67c6adbcc5cb
edilfonso2019 <benjaminedilfonso@gmail.com>
2022-08-23 10:04:29 -0500

    Se agrega el proyecto a desarrollar

---
+++ conver.js
@@ -0,0 +1,27 @@
+var x;
+x = prompt("Ingrese numero uno: ");
+
+function convertirEnteroARomano(numero) {
+    if (typeof numero != 'number' || !Number.isInteger(numero)) {
+        return null;
+    }
+
+    const ROMANOS = ['','C','CC','CCC','CD','D','DC','DCC','DCCC','CM',
+    '','X','XX','XXX','XL','L','LX','LXX','LXXX','XC',
+    '','I','II','III','IV','V','VI','VII','VIII','IX'];
+
+    let digitos = String(numero).split('');
+    let romano = '';
+    let i = 3;
+
+    while (i--) {
+        romano = (ROMANOS[+digitos.pop() + (i * 10)] || '') + romano
+    }
+
+    return Array(+digitos.join('') + 1).join('M') + romano;
+}
+
+
+console.log(convertirEnteroARomano(1));  // XI
+console.log(convertirEnteroARomano(23));  // XXIII
+console.log(convertirEnteroARomano(8));  // VIII
