afa23d7ea09a6605b9bdcd8a23c3afc88f61365f
edilfonso2019 <benjaminedilfonso@gmail.com>
2022-08-23 23:51:57 -0500

    modifique parte final

--- conver.js
+++ conver.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 
-=======
->>>>>>> 98f6dc2dd7ddbddab1a34d152055be1d872920e3
 
 function convertirEnteroARomano(numero) {
     if (typeof numero != 'number' || !Number.isInteger(numero)) {
@@ -24,9 +21,14 @@ function convertirEnteroARomano(numero) {
 }
 
 
-console.log(convertirEnteroARomano(1));  // XI
-console.log(convertirEnteroARomano(8));  // XI
-console.log(convertirEnteroARomano(23));  // XXIII
+console.log(convertirEnteroARomano(1));  // I
+console.log(convertirEnteroARomano(2));  // II
+console.log(convertirEnteroARomano(3));  // III
+console.log(convertirEnteroARomano(4));  // IV
+console.log(convertirEnteroARomano(5));  // V
+console.log(convertirEnteroARomano(6));  // VI
+console.log(convertirEnteroARomano(7));  // VI
 console.log(convertirEnteroARomano(8));  // VIII
-console.log(convertirEnteroARomano(8));  // VIII
+console.log(convertirEnteroARomano(9));  // IX
+console.log(convertirEnteroARomano(10));  // X
 
